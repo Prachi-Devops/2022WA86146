d1ddb958cee6d744e33379e1022e93ef0e30a46a
Prachi <prachisaini0010@gmail.com>
2026-09-25 00:03:58 +0530

    Modify source code for CI

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from DevOps Continuous Integration!");
+        System.out.println("Hello from DevOps Continuous Integration - Version2!");
     }
 }
